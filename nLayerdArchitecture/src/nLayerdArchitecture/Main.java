dae66f5514b92c6fe493b21f2d1edfeef3d583a7
gulsenkeskin <gulsenkeskin2@gmail.com>
2021-10-23 00:19:35 +0300

    :pizza: update main

--- nLayerdArchitecture/src/nLayerdArchitecture/Main.java
+++ nLayerdArchitecture/src/nLayerdArchitecture/Main.java
@@ -2,6 +2,7 @@ package nLayerdArchitecture;
 
 import nLayerdArchitecture.business.abstracts.ProductService;
 import nLayerdArchitecture.business.concretes.ProductManager;
+import nLayerdArchitecture.dataAccess.concretes.AbcProductDao;
 import nLayerdArchitecture.dataAccess.concretes.HibernateProductDao;
 import nLayerdArchitecture.entities.concretes.Product;
 
@@ -10,9 +11,15 @@ public class Main {
 	public static void main(String[] args) {
 		//ToDo: Spring IoC ile çözülecek
 		ProductService productService = new ProductManager(new HibernateProductDao());
-		
-		Product product= new Product(1,1,"Silgi",12,60);
+		Product product1= new Product(1,1,"Silgi",12,60);
+		Product product= new Product(1,2,"Silgi",12,60);
+		productService.add(product1);
 		productService.add(product);
+
+		ProductService productServiceAbc = new ProductManager(new AbcProductDao());
+		productServiceAbc.add(product);
+
+	
 	}
 }
 
