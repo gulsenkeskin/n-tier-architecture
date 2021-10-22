a436395836d760ed6a2f9f305720022f4575c68a
gulsenkeskin <gulsenkeskin2@gmail.com>
2021-10-22 23:58:51 +0300

    :herb:  ProductManager implements ProductService
    :herb:  ProductManager implements ProductService
    
    :cherry_blossom:

--- nLayerdArchitecture/src/nLayerdArchitecture/business/abstracts/ProductService.java
+++ nLayerdArchitecture/src/nLayerdArchitecture/business/abstracts/ProductService.java
@@ -1,5 +1,11 @@
 package nLayerdArchitecture.business.abstracts;
 
+import java.util.List;
+
+import nLayerdArchitecture.entities.concretes.Product;
+
 public interface ProductService {
+	void add(Product product);
+	List<Product> getAll();
 
 }
