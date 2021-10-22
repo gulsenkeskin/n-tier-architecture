0df32979334c96ffa47d9ed233946aab250f5c23
gulsenkeskin <gulsenkeskin2@gmail.com>
2021-10-22 23:28:27 +0300

    Update HibernateProductDao :pineapple:
    HibernateProductDao implemented method :green_apple:

--- nLayerdArchitecture/src/nLayerdArchitecture/dataAccess/concretes/HibernateProductDao.java
+++ nLayerdArchitecture/src/nLayerdArchitecture/dataAccess/concretes/HibernateProductDao.java
@@ -1,7 +1,40 @@
 package nLayerdArchitecture.dataAccess.concretes;
+import java.util.List;
+
 import nLayerdArchitecture.dataAccess.abstracts.ProductDao;
+import nLayerdArchitecture.entities.concretes.Product;
 
 public class HibernateProductDao implements ProductDao {
+
+	@Override
+	public void add(Product product) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Product product) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Product product) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Product get(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Product> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	 
 
 }
