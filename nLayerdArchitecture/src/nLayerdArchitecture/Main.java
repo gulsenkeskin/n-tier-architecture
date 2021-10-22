39c18682c4f69ddfd35880170e09f83e72ff9238
gulsenkeskin <gulsenkeskin2@gmail.com>
2021-10-23 00:13:49 +0300

    :pineapple: update main

--- nLayerdArchitecture/src/nLayerdArchitecture/Main.java
+++ nLayerdArchitecture/src/nLayerdArchitecture/Main.java
@@ -1,10 +1,19 @@
 package nLayerdArchitecture;
 
+import nLayerdArchitecture.business.abstracts.ProductService;
+import nLayerdArchitecture.business.concretes.ProductManager;
+import nLayerdArchitecture.dataAccess.concretes.HibernateProductDao;
+import nLayerdArchitecture.entities.concretes.Product;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//ToDo: Spring IoC ile çözülecek
+		ProductService productService = new ProductManager(new HibernateProductDao());
+		
+		Product product= new Product(1,1,"Silgi",12,60);
+		productService.add(product);
 	}
-
 }
+
+//not: interface'ler somut sýnýflarýn referansýný tutabilir
