a4fc2be414e3ad95b33e420d2f48b00c723339e2
gulsenkeskin <gulsenkeskin2@gmail.com>
2021-10-22 23:22:40 +0300

    HibernateProductDao implements ProductDao
    :maple_leaf:

--- nLayerdArchitecture/src/nLayerdArchitecture/dataAccess/concretes/HibernateProductDao.java
+++ nLayerdArchitecture/src/nLayerdArchitecture/dataAccess/concretes/HibernateProductDao.java
@@ -1,5 +1,10 @@
 package nLayerdArchitecture.dataAccess.concretes;
+import nLayerdArchitecture.dataAccess.abstracts.ProductDao;
 
-public interface HibernateProductDao {
+public class HibernateProductDao implements ProductDao {
+	 
 
 }
+
+
+//Product'ýn veri eriþim kodlarýný yazmak için gerekli olan sýnýf
