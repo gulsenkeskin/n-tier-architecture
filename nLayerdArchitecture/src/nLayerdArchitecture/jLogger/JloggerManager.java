954df21f2b1d0d80b58ded743c16c0e439328fea
gulsenkeskin <gulsenkeskin2@gmail.com>
2021-10-23 00:44:27 +0300

    :night_with_stars: update JLoggerManager

--- nLayerdArchitecture/src/nLayerdArchitecture/jLogger/JloggerManager.java
+++ nLayerdArchitecture/src/nLayerdArchitecture/jLogger/JloggerManager.java
@@ -2,7 +2,7 @@ package nLayerdArchitecture.jLogger;
 
 public class JloggerManager {
 	public void log(String message) {
-		System.out.println("J Logger ile loglandý ");
+		System.out.println("J Logger ile loglandý: "+ message);
 	}
 
 }
