9cbafc40ba5274d5d5ef9d9ff4ce1cbdfa6ae2c7
gulsenkeskin <gulsenkeskin2@gmail.com>
2021-10-23 00:13:21 +0300

    :purple_heart:  add ProductManager

--- nLayerdArchitecture/src/nLayerdArchitecture/business/concretes/ProductManager.java
+++ nLayerdArchitecture/src/nLayerdArchitecture/business/concretes/ProductManager.java
@@ -1,5 +1,37 @@
 package nLayerdArchitecture.business.concretes;
 
-public class ProductManager {
+import java.util.List;
+
+import nLayerdArchitecture.business.abstracts.ProductService;
+import nLayerdArchitecture.dataAccess.abstracts.ProductDao;
+import nLayerdArchitecture.entities.concretes.Product;
+
+public class ProductManager implements ProductService {
+	
+	//dependency injection
+	private ProductDao productDao;
+
+	public ProductManager(ProductDao productDao) {
+		super();
+		this.productDao = productDao;
+	}
+
+	@Override
+	public void add(Product product) {
+		// here write business code		
+		//example:
+		if(product.getCategoryId()==1) {
+			System.out.println("No products are accepted in this category.");
+			return;
+		}
+		this.productDao.add(product);
+
+	}
+
+	@Override
+	public List<Product> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
